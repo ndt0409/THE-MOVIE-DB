152cc376ffb65d5265e10c94e995fcb5bafb23d6
NDT <vivucenter@gmail.com>
2022-09-20 15:59:03 +0700

    create ui home screen

---
+++ app/src/main/java/com/ndt/themoviedb/ui/home/HomeFragment.kt
@@ -0,0 +1,10 @@
+package com.ndt.themoviedb.ui.home
+
+import com.ndt.themoviedb.databinding.FragmentHomeBinding
+import com.ndt.themoviedb.ui.base.BaseFragment
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate){
+    override fun initData() {
+        TODO("Not yet implemented")
+    }
+}
--- app/src/main/java/com/ndt/themoviedb/ui/navigation/ContainerFragment.kt
+++ app/src/main/java/com/ndt/themoviedb/ui/navigation/ContainerFragment.kt
@@ -1,11 +1,68 @@
 package com.ndt.themoviedb.ui.navigation
 
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentTransaction
+import com.ndt.themoviedb.R
 import com.ndt.themoviedb.databinding.FragmentContainerBinding
 import com.ndt.themoviedb.ui.base.BaseFragment
+import com.ndt.themoviedb.ui.home.HomeFragment
 
 class ContainerFragment :
     BaseFragment<FragmentContainerBinding>(FragmentContainerBinding::inflate) {
+    private var startingPosition = 0
+
     override fun initData() {
-        //TODO implement later
+        TODO("Not yet implemented")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        loadFragment(HomeFragment(), startingPosition)
+    }
+
+    private fun loadFragment(fragment: Fragment, position: Int): Boolean {
+        activity?.run {
+            when {
+                startingPosition > position -> {
+                    slideLeftToRight(fragment)
+                }
+                startingPosition < position -> {
+                    slideRightToLeft(fragment)
+                }
+                else -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container_frame_layout, fragment)
+                        .commit()
+                }
+            }
+        }
+        startingPosition = position
+        return true
     }
 }
+
+private fun FragmentActivity.slideRightToLeft(fragment: Fragment) {
+    supportFragmentManager.beginTransaction().setCustomAnimations(
+        R.anim.right_to_left,
+        R.anim.exit_right_to_left,
+        R.anim.left_to_right,
+        R.anim.exit_left_to_right
+    )
+        .replace(R.id.container_frame_layout, fragment)
+        .commit()
+}
+
+private fun FragmentActivity.slideLeftToRight(fragment: Fragment) {
+    supportFragmentManager.beginTransaction().setCustomAnimations(
+        R.anim.left_to_right,
+        R.anim.exit_left_to_right,
+        R.anim.right_to_left,
+        R.anim.exit_right_to_left
+    )
+        .replace(R.id.container_frame_layout, fragment)
+        .commit()
+}
+
+
